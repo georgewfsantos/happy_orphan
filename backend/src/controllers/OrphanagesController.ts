d7272940c6b39c1840f0ee68858d9d71cca0e8d2
George W F Santos <gweverton21@hotmail.com>
2020-10-17 19:21:28 -0300

    add unsaved minor changes

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -21,7 +21,7 @@ export default {
 
   async create(request: Request, response: Response){
     const {name, latitude, longitude, about, instructions, opening_hours, open_on_weekends} = request.body;
-
+    
     const orphanagesRepository = getRepository(Orphanage);
 
     const orphanage = orphanagesRepository.create({
